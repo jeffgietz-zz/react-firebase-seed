c6798e1bd6ae574ece78ef0d4e59402c8f26c4a4
jgietz <jgietz@console.com.au>
2017-03-27 08:42:17 +1100

    Took out bootstrap folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './components';
 // import './index.css';
-import './styles/bootstrap/bootstrap.css';
+import './styles/bootstrap.css';
 import './styles/App.css';
 
 // import * as firebase from 'firebase';
